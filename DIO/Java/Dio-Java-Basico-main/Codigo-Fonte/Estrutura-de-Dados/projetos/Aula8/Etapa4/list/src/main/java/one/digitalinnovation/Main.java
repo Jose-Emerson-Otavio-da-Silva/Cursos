04267e3467b21810f7e45e50f74bc56247e1c907
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-02-01 11:29:49 -0300

    .

--- DIO/Java/Dio-Java-Basico-main/Codigo-Fonte/Estrutura-de-Dados/projetos/Aula8/Etapa4/list/src/main/java/one/digitalinnovation/Main.java
+++ DIO/Java/Dio-Java-Basico-main/Codigo-Fonte/Estrutura-de-Dados/projetos/Aula8/Etapa4/list/src/main/java/one/digitalinnovation/Main.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String args[]){
+    /**
+     * @param args
+     */
+    public static void main(String args[]) {
 
         List<Carro> listCarros = new ArrayList<>();
 
@@ -14,14 +17,15 @@ public class Main {
         listCarros.add(new Carro("Fiat"));
         listCarros.add(new Carro("Peugeot"));
 
+        // Verifica se contem o item na lista
         System.out.println(listCarros.contains(new Carro("Ford")));
-
+        // Retorna o item da lista
         System.out.println(listCarros.get(2));
-
+        // Retorna o index do item na lista
         System.out.println(listCarros.indexOf(new Carro("Fiat")));
-
+        // Remove o item da lista
         System.out.println(listCarros.remove(2));
-
+        // Exibe a lista
         System.out.println(listCarros);
     }
 
