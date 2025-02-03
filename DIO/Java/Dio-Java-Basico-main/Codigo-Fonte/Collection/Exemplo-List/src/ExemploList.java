9a11876b5d670f10584e7377f9f879c5dc45d4e6
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-02-03 18:34:29 -0300

    Criando o projeto Exemplo List do aula sobre collection

---
+++ DIO/Java/Dio-Java-Basico-main/Codigo-Fonte/Collection/Exemplo-List/src/ExemploList.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class ExemploList {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Crie uma lista e adicione as sete notas: ");
+
+        List<Double> notas = new ArrayList<>();
+        notas.add(7.0);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5.0);
+        notas.add(7.0);
+        notas.add(0.0);
+        notas.add(3.6);
+        System.out.println(notas.toString());
+    }
+}
