548cf7019afdc854d4f9d47919b6bd8d321b86a9
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-02-05 19:08:38 -0300

    .

---
+++ DIO/Java/Dio-Java-Basico-main/Codigo-Fonte/Collection/Exemplo-Set/src/ExemploOrdenacaoSet.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ExemploOrdenacaoSet {
+    public static void main(String[] args) throws Exception {
+
+    }
+}
+
+class Serie {
+    private String nome;
+    private String genero;
+    private Integer tempoEpisodio;
+
+    public Serie(String nome, String genero, Integer tempoEpisodio) {
+        this.nome = nome;
+        this.genero = genero;
+        this.tempoEpisodio = tempoEpisodio;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public Integer getTempoEpisodio() {
+        return tempoEpisodio;
+    }
+
+    @Override
+    public String toString() {
+        return "Serie{" +
+                "nome='" + nome + '\'' +
+                ", genero='" + genero + '\'' +
+                ", tempoEpisodio=" + tempoEpisodio +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Serie serie = (Serie) o;
+
+        return nome.equals(serie.nome) && genero.equals(serie.genero) && tempoEpisodio.equals(serie.tempoEpisodio);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, genero, tempoEpisodio);
+    }
+
+}
