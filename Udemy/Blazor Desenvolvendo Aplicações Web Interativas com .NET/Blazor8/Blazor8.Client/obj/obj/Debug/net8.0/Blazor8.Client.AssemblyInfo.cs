90faa85c26fce3fb1b0395930731caa3812c5617
Jose-Emerson-Otavio-da-Silva <emersonr8@hotmail.com>
2025-02-28 09:22:45 -0300

    .

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Blazor8/Blazor8.Client/obj/obj/Debug/net8.0/Blazor8.Client.AssemblyInfo.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Blazor8/Blazor8.Client/obj/obj/Debug/net8.0/Blazor8.Client.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Blazor8")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Blazor8.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d01f379987c8c4a1042245cb25b80750fcafdeb3")]
-[assembly: System.Reflection.AssemblyProductAttribute("Blazor8")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Blazor8")]
+[assembly: System.Reflection.AssemblyProductAttribute("Blazor8.Client")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Blazor8.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
