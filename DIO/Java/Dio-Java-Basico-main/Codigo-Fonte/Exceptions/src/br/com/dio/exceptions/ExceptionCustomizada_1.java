ef5b9fc3591f6ee2b61facfde355d6ebfd17d8f6
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-03-10 19:31:20 -0300

    Adicionar tratamento de exceções e comentários na classe ExceptionCustomizada_1

--- DIO/Java/Dio-Java-Basico-main/Codigo-Fonte/Exceptions/src/br/com/dio/exceptions/ExceptionCustomizada_1.java
+++ DIO/Java/Dio-Java-Basico-main/Codigo-Fonte/Exceptions/src/br/com/dio/exceptions/ExceptionCustomizada_1.java
@@ -5,18 +5,21 @@ import java.io.*;
 
 public class ExceptionCustomizada_1 {
     public static void main(String[] args) {
+        // Solicita ao usuário o nome do arquivo a ser exibido
         String nomeDoArquivo = JOptionPane.showInputDialog("Nome do arquivo a ser exibido: ");
 
+        // Chama o método para imprimir o conteúdo do arquivo no console
         imprimirArquivoNoConsole(nomeDoArquivo);
         System.out.println("\nCom exception ou não, o programa continua...");
     }
 
     public static void imprimirArquivoNoConsole(String nomeDoArquivo) {
-
         try {
+            // Lê o arquivo e obtém um BufferedReader
             BufferedReader br = lerArquivo(nomeDoArquivo);
             String line = br.readLine();
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            // Lê e escreve cada linha do arquivo no console
             do {
                 bw.write(line);
                 bw.newLine();
@@ -25,10 +28,10 @@ public class ExceptionCustomizada_1 {
             bw.flush();
             br.close();
         } catch (ImpossivelAberturaDeArquivoException e) {
-            JOptionPane.showMessageDialog(null,
-                    e.getMessage());
-            //e.printStackTrace();
+            // Trata a exceção personalizada e exibe uma mensagem de erro
+            JOptionPane.showMessageDialog(null, e.getMessage());
         } catch (IOException ex) {
+            // Trata outras exceções de I/O e exibe uma mensagem de erro genérica
             JOptionPane.showMessageDialog(null,
                     "Ocorreu um erro não esperado, por favor, fale com o suporte." + ex.getMessage());
             ex.printStackTrace();
@@ -36,18 +39,19 @@ public class ExceptionCustomizada_1 {
     }
 
     public static BufferedReader lerArquivo(String nomeDoArquivo) throws ImpossivelAberturaDeArquivoException {
-
         File file = new File(nomeDoArquivo);
         try {
+            // Tenta abrir o arquivo e retorna um BufferedReader
             return new BufferedReader(new FileReader(nomeDoArquivo));
         } catch (FileNotFoundException e) {
+            // Lança uma exceção personalizada se o arquivo não for encontrado
             throw new ImpossivelAberturaDeArquivoException(file.getName(), file.getPath());
         }
     }
 }
 
+// Classe de exceção personalizada para tratar arquivos não encontrados
 class ImpossivelAberturaDeArquivoException extends Exception {
-
     private String nomeDoArquivo;
     private String diretorio;
 
@@ -57,15 +61,13 @@ class ImpossivelAberturaDeArquivoException extends Exception {
         this.diretorio = diretorio;
     }
 
-    /*@Override
-    public String toString() {
-        return "ImpossivelAberturaDeArquivoException{" +
-                "nomeDoArquivo='" + nomeDoArquivo + '\'' +
-                ", diretorio='" + diretorio + '\'' +
-                '}';
-    }*/
-
+    /*
+     * @Override
+     * public String toString() {
+     * return "ImpossivelAberturaDeArquivoException{" +
+     * "nomeDoArquivo='" + nomeDoArquivo + '\'' +
+     * ", diretorio='" + diretorio + '\'' +
+     * '}';
+     * }
+     */
 }
-
-
-
