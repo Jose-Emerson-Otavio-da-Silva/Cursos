a62fb4cf7fe92b1b4e37e157b44bd16df71a2539
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-03-11 20:35:28 -0300

    Extraido pasta de material

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/obj/Debug/net8.0/Gestao.AssemblyInfo.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/obj/Debug/net8.0/Gestao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28e9e85261de476873e3161d689a98618e141a2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f44a2eb957820d75df014455986fa8f7fb60e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
