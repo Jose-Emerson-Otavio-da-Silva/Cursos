ab0e2b752dba0061a3eb8a05c7d2f977c4e85586
Jose-Emerson-Otavio-da-Silva <emersonr8@hotmail.com>
2025-03-17 16:45:54 -0300

    Ajustado estética da tela de login

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/obj/Debug/net8.0/Gestao.AssemblyInfo.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/obj/Debug/net8.0/Gestao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d27ab70023224dc7fba5fa7ac69d811f5fdacf30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03df7aaa1bbaf689361419aba12c190680e13ce4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
