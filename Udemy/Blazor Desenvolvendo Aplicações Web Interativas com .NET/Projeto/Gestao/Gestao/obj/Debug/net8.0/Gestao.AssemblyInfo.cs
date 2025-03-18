90691cdd1cdabb40865c1dc47ddce80c277aad8c
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-03-18 20:13:24 -0300

    Atualizar arquivos binários e de depuração do projeto Gestao.Client

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/obj/Debug/net8.0/Gestao.AssemblyInfo.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/obj/Debug/net8.0/Gestao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d91206db210d8431d48da151d60c79d11190f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9863f3bdc9757501c4ddefd44a95f58f204bc8c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
