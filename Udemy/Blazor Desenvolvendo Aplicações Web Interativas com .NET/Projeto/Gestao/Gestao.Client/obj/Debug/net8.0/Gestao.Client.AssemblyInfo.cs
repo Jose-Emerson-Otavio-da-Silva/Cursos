38e520dd8af4686a1abf0fdcaaf1d59a96dceffa
Jose-Emerson-Otavio-da-Silva <emersonr8@hotmail.com>
2025-04-02 11:45:26 -0300

    .

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Client/obj/Debug/net8.0/Gestao.Client.AssemblyInfo.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Client/obj/Debug/net8.0/Gestao.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e77a7bf17c1b2fac6918327f41d1a7f90d2d237")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fae4f451017ebeccfc5e70017d605155d7b9618")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
