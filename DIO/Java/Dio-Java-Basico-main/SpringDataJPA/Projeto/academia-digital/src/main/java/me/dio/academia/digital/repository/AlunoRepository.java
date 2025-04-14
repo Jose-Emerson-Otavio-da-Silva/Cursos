4c5e322f0bbff676ccaa73c0e8adaa2c3d4c41e0
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-04-14 20:36:49 -0300

    Implementação do AlunoController e AlunoServiceImpl, incluindo métodos para criação e listagem de alunos. Atualização do AlunoRepository e AlunoForm para suportar as novas funcionalidades. Ajustes no application.yml para configuração do Hibernate.

--- DIO/Java/Dio-Java-Basico-main/SpringDataJPA/Projeto/academia-digital/src/main/java/me/dio/academia/digital/repository/AlunoRepository.java
+++ DIO/Java/Dio-Java-Basico-main/SpringDataJPA/Projeto/academia-digital/src/main/java/me/dio/academia/digital/repository/AlunoRepository.java
@@ -1,4 +1,14 @@
 package me.dio.academia.digital.repository;
 
-public interface AlunoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.academia.digital.entity.Aluno;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+    // Aqui você pode adicionar métodos personalizados, se necessário
+    // Por exemplo:
+    // List<Aluno> findByNome(String nome);
+    // Optional<Aluno> findByCpf(String cpf);
 }
