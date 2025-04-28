1608162fb2cadf55b7c9822638ad0f6b102226ab
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-04-28 20:21:22 -0300

    Adicionadas validações nos formulários de Aluno, Avaliação Física e Matrícula; configurado nível de log do Hibernate.

--- DIO/Java/Dio-Java-Basico-main/SpringDataJPA/Projeto/academia-digital/src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
+++ DIO/Java/Dio-Java-Basico-main/SpringDataJPA/Projeto/academia-digital/src/main/java/me/dio/academia/digital/entity/form/AvaliacaoFisicaForm.java
@@ -1,17 +1,28 @@
 package me.dio.academia.digital.entity.form;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AvaliacaoFisicaForm {
 
+  @Positive(message = "O Id do aluno deve ser positivo.")
   private Long alunoId;
 
+  @NotNull(message = "Preencha o campo corretamente.")
+  @Positive(message = "'${validatedValue}' deve ser positivo.")
   private double peso;
 
+  @NotNull(message = "Preencha o campo corretamente.")
+  @Positive(message = "'${validatedValue}' deve ser positivo.")
+  @DecimalMin(value = "150", message = "'${validatedValue}' deve ser no minimo {value}.")
   private double altura;
 }
