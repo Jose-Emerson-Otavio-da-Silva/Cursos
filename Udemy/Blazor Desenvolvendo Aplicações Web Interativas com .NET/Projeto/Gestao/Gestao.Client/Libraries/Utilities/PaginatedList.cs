25cb0bfde2f240187f2c33f507d0b912cb3c5dcb
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-08-14 19:50:16 -0300

    Melhorada a documentação e comentários na classe EmailSender, detalhando o envio de e-mails e suas funcionalidades.

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Client/Libraries/Utilities/PaginatedList.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Client/Libraries/Utilities/PaginatedList.cs
@@ -7,16 +7,27 @@ namespace Gestao.Client.Libraries.Utilities
 {
     public class PaginatedList<T>
     {
+        // Construtor que inicializa a lista paginada com os itens, índice da página atual e total de páginas
         public PaginatedList(List<T> items, int pageIndex, int totalPages)
         {
             Items = items;
             PageIndex = pageIndex;
             TotalPages = totalPages;
         }
+
+        // Lista de itens da página atual
         public List<T> Items { get; } = new List<T>();
+
+        // Índice da página atual (começa em 1)
         public int PageIndex { get; }
+
+        // Total de páginas disponíveis
         public int TotalPages { get; }
+
+        // Indica se há uma página anterior
         public bool HasPreviousPage => PageIndex > 1;
+
+        // Indica se há uma próxima página
         public bool HasNextPage => PageIndex < TotalPages;
     }
 }
--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/Libraries/Mail/EmailSender.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/Libraries/Mail/EmailSender.cs
@@ -3,48 +3,66 @@ using Gestao.Domain;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 
-
 namespace Gestao.Libraries.Mail
 {
+    /// <summary>
+    /// Classe EmailSender responsável por enviar e-mails
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="configuration"></param>
+    /// <param name="smtp"></param>
     public class EmailSender(ILogger<EmailSender> logger, IConfiguration configuration, SmtpClient smtp) : IEmailSender<ApplicationUser>
     {
+        // Instância do logger para registrar eventos de envio de e-mails
         private readonly ILogger logger = logger;
+
+        // Cliente SMTP para envio de e-mails
         private readonly SmtpClient smtp = smtp;
+
+        // Instância de configuração para acessar as configurações da aplicação
         private readonly IConfiguration configuration = configuration;
 
+        // Envia um e-mail com o link de confirmação para o usuário
         public Task SendConfirmationLinkAsync(ApplicationUser user, string email,
             string confirmationLink) => SendEmailAsync(email, "Confirme seu e-mail",
             "<html lang=\"en\"><head></head><body>Por favor confirme seu cadastro clicando no link: " +
             $"<a href='{confirmationLink}'>Confirmar</a>.</body></html>");
 
+        // Envia um e-mail com o link para redefinição de senha para o usuário
         public Task SendPasswordResetLinkAsync(ApplicationUser user, string email,
             string resetLink) => SendEmailAsync(email, "Resetar senha",
             "<html lang=\"en\"><head></head><body>Resete sua senha clicando no link: " +
             $"<a href='{resetLink}'>Resetar senha</a>.</body></html>");
 
+        // Envia um e-mail com o código para redefinição de senha para o usuário
         public Task SendPasswordResetCodeAsync(ApplicationUser user, string email,
             string resetCode) => SendEmailAsync(email, "Resetar senha",
-            "<html lang=\"en\"><head></head><body>Reset sua senha " +
+            "<html lang=\"en\"><head></head><body>Resete sua senha " +
             $"usando o seguinte código:<br>{resetCode}</body></html>");
 
+        // Envia um e-mail de forma assíncrona
         public async Task SendEmailAsync(string toEmail, string subject, string message)
         {
             await Execute(subject, message, toEmail);
         }
 
+        // Executa o processo de envio do e-mail
         public async Task Execute(string subject, string message,
             string toEmail)
         {
+            // Cria uma nova mensagem de e-mail
             var mailMessage = new MailMessage();
-            mailMessage.From = new MailAddress(configuration.GetValue<string>("EmailSender:User")!);
-            mailMessage.To.Add(new MailAddress(toEmail));
-            mailMessage.Subject = subject;
-            mailMessage.Body = message;
-            mailMessage.IsBodyHtml = true;
+            mailMessage.From = new MailAddress(configuration.GetValue<string>("EmailSender:User")!); // Endereço de e-mail do remetente
+            mailMessage.To.Add(new MailAddress(toEmail)); // Endereço de e-mail do destinatário
+            mailMessage.Subject = subject; // Assunto do e-mail
+            mailMessage.Body = message; // Corpo do e-mail
+            mailMessage.IsBodyHtml = true; // Indica que o corpo do e-mail é HTML
 
+            // Envia o e-mail usando o cliente SMTP
             await smtp.SendMailAsync(mailMessage);
 
-            logger.LogInformation("Email to {EmailAddress} sent!", toEmail);
+            // Registra o evento de envio do e-mail
+            logger.LogInformation("E-mail para {EmailAddress} enviado!", toEmail);
         }
     }
 }
