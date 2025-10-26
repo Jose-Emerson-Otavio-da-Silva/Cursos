71d4c601615d7f585cff6d7c02f076121db679e1
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-10-26 20:27:01 -0300

    Corrigido botoes de paginacao

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Client/obj/Debug/net8.0/Gestao.Client.AssemblyInfo.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Client/obj/Debug/net8.0/Gestao.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a884a5cc754ff71c29e02bdc76d5f2f98940bbc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4df783a52ec12674a9a1e069c06f8109b9152108")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
