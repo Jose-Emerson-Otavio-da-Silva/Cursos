84dd4533d99e5b87aaf4af282d916cde577f5b1f
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-10-29 19:15:40 -0300

    adicionado spinner de loading no campo cep na tela de cadastro > empresa

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/obj/Debug/net8.0/Gestao.AssemblyInfo.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/obj/Debug/net8.0/Gestao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3385a7a8a7d6188643ea16b06645e9771fab4429")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+756600322385ee33e15f92788b8cdd992320541d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
