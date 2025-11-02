51f7aa735e99ca1ebb24f538bb9b264663a0021e
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-11-01 23:29:00 -0300

    Concluido tela de alteração de cadastro de empresa.

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Domain/obj/Debug/net8.0/Gestao.Domain.AssemblyInfo.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Domain/obj/Debug/net8.0/Gestao.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103df7b02a6e4ea06fb7ce532e83946d8a8a2bf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfa784a2265c89728ff00230b923918e4dd51cf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
