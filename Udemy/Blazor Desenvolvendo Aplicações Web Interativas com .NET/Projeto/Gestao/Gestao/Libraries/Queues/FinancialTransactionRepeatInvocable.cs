eac3304d3600ee1d1aa9a31bdfd719a38a49322c
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-12-02 13:37:05 -0300

    Inciado classe FinancialTransactionRepeatInvocable

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/Libraries/Queues/FinancialTransactionRepeatInvocable.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao/Libraries/Queues/FinancialTransactionRepeatInvocable.cs
@@ -1,15 +1,50 @@
 using Coravel.Invocable;
 using Gestao.Domain;
+using Gestao.Domain.Repositories;
+using Microsoft.VisualBasic;
 
 namespace Gestao.Libraries.Queues
 {
     public class FinancialTransactionRepeatInvocable : IInvocable, IInvocableWithPayload<FinancialTransaction>
     {
+        private IFinancialTransactionRepository _repository;
+        public FinancialTransactionRepeatInvocable(IFinancialTransactionRepository repository)
+        {
+            _repository = repository;
+        }
         public FinancialTransaction Payload { get; set; }
 
-        Task IInvocable.Invoke()
+        public async Task Invoke()
         {
-            throw new NotImplementedException();
+            var startPoint = 1;
+            RegisterNewTransaction(startPoint);
+        }
+
+        private void RegisterNewTransaction(int startPoint)
+        {
+            if (Payload.Repeat != Domain.Enums.Recurrence.None)
+            {
+                var repeatTimes = Payload.RepeatTimes - 1;
+
+                for (int i = startPoint; i <= repeatTimes; i++)
+                {
+                    var financial = new FinancialTransaction();
+                    financial.TypeFinancialTransaction = Payload.TypeFinancialTransaction;
+                    financial.Description = Payload.Description;
+                    financial.ReferenceDate = Payload.ReferenceDate;
+                    financial.DueDate = Payload.DueDate;
+                    financial.Amount = Payload.Amount;
+                    financial.RepeatGroup = Payload.Id;
+                    financial.Repeat = Domain.Enums.Recurrence.None;
+                    financial.RepeatTimes = null;
+                    financial.CreatedAt = DateTimeOffset.Now;
+
+                    financial.CompanyId = Payload.CompanyId;
+                    financial.AccountId = Payload.AccountId;
+                    financial.CategoryId = Payload.CategoryId;
+                    _repository.Add(financial);
+                }
+            }
         }
     }
 }
