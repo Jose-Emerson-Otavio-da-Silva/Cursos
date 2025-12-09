432ea95623062bc5645d95479566b665d145ad39
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2025-12-09 15:52:36 -0300

    Iniciado painel da tela Home

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Client/obj/Debug/net8.0/Gestao.Client.AssemblyInfo.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Client/obj/Debug/net8.0/Gestao.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01518bdeaee39c291a7775b4d9e1c2f0e3b1a1ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab3054fb4a834269a878f50a382658e499e348c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
