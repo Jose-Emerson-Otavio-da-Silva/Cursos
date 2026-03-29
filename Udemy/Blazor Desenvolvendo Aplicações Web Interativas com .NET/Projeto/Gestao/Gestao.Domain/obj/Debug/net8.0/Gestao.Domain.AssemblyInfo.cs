b6adcd8ff94617e490fa847c55fb4be08ab16603
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2026-03-28 22:53:03 -0300

    .Net Maui - Criado App 01 (NumeroDaSorte)

--- Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Domain/obj/Debug/net8.0/Gestao.Domain.AssemblyInfo.cs
+++ Udemy/Blazor Desenvolvendo Aplicações Web Interativas com .NET/Projeto/Gestao/Gestao.Domain/obj/Debug/net8.0/Gestao.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98cf9c89587072b107622fa3bd812f45638ffb35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff1137b20d756f3655cb3d9369f2cf2a62584a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
